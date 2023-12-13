0bdf7d329a87c8df7c191a7217b708c0f74b8f82
AwaisYounas72 <151017136+AwaisYounas72@users.noreply.github.com>
2023-12-13 17:38:34 +0500

    Add files via upload

---
+++ Challenege3/client/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const [randomNumber, setRandomNumber] = useState(0);
+   
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:3000/api/random');
+      // setNumber(response.data.randomNumber);
+      setRandomNumber(response.data.randomNumber);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (randomNumber < 0) {
+      fetchData();
+    }
+  }, [randomNumber]);
+
+const decreaseNum = num => {
+  if(num < 1 ){
+    fetchData();
+  }
+  setRandomNumber(num => num-1);
+}
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      decreaseNum(randomNumber);
+      }, 1000);
+      console.log(randomNumber, 'ques')
+    return () => clearInterval(interval);
+  }, [randomNumber]);
+
+  const fetchData = async () => {
+    const response = await axios.get('http://localhost:3000/api/random');
+    // setNumber(response.data.randomNumber);
+    setRandomNumber(response.data.randomNumber);
+  
+  };
+  const arrayWithIndices = Array.from({ length: randomNumber }, (_, index) => index);
+  return (
+    <div className="App bg-blue-800 w-[100%] min-h-screen  flex justify-center items-center">
+     
+      <div className="  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        {arrayWithIndices.map((num,index) => 
+        {return (<div key={index} className='h-[10rem] w-[10rem] bg-blue-600 p-5 rounded-lg shadow-lg  border-white border flex justify-center items-center text-white font-bold mx-5 my-3'>{num + 1}</div>)})}
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
